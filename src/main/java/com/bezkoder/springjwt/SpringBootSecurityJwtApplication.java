9eef7c927e68d80b143870c42cdc81d9c2e06590
sandeepkumarshukla <sandeep.shukla@gmail.com>
2022-01-05 14:09:40 +0100

    Initial commit

---
+++ src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtApplication {
+//https://www.bezkoder.com/spring-boot-jwt-authentication/
+	public static void main(String[] args) {
+    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+	}
+
+}
